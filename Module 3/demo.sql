b4a216aaa475f35b06b2927f2fa0bf009400d8fc
hongquan1105 <quantran11052005@gmail.com>
2024-10-31 20:31:53 +0700

    baitap

--- Module 3/demo.sql
+++ Module 3/demo.sql
@@ -1,32 +1,11 @@
-create database c0624;
-use c0624;
--- Tao bang student cos name, dob, gender, point\
-CREATE TABLE student (
-    name VARCHAR(50),
-    dob DATE,
-    gender BIT,
-    point DOUBLE
-);
+create database demo;
+use demo;
 
--- Thao tac voi du lieu trong bang DML
-insert into student (name, dob, gender, point)
-values ("Tran Quan", "2005-11-5", 1, 5),
-		("Thanh Cong", "2000-01-01", 1, 6),
-		("Van Tai", "2004-01-01", 1, 7);
+-- Tao bang student co id,name,age,country
+CREATE TABLE student (id int, name VARCHAR(200), age int, country varchar(50));
 
-SELECT 
-    *
-FROM
-    student;
-
-set sql_safe_updates = 0;-- Tat chuc nang safe update, vi safe update chi cho phep update voi khoa chinh (main key)
-
-UPDATE student 
-SET 
-    point = 7
-WHERE
-    name = 'Thanh Cong';
-
--- DELETE FROM student 
--- WHERE
---     name = 'Van Tai';
+-- Them thong tin vao bang Student da tao
+insert into student (id, name, age, country)
+values (1, "Tran Quan", 19, "Da Nang"),
+		(2, "Thanh Cong", 33 , "Quang Nam"),
+		(3, "Van Tai", 20, "Da Nang");
---
+++ Module 3/ss1_co_so_du_lieu_quan_he.sql
@@ -0,0 +1,32 @@
+create database c0624;
+use c0624;
+-- Tao bang student cos name, dob, gender, point\
+CREATE TABLE student (
+    name VARCHAR(50),
+    dob DATE,
+    gender BIT,
+    point DOUBLE
+);
+
+-- Thao tac voi du lieu trong bang DML
+insert into student (name, dob, gender, point)
+values ("Tran Quan", "2005-11-5", 1, 5),
+		("Thanh Cong", "2000-01-01", 1, 6),
+		("Van Tai", "2004-01-01", 1, 7);
+
+SELECT 
+    *
+FROM
+    student;
+
+set sql_safe_updates = 0;-- Tat chuc nang safe update, vi safe update chi cho phep update voi khoa chinh (main key)
+
+UPDATE student 
+SET 
+    point = 7
+WHERE
+    name = 'Thanh Cong';
+
+DELETE FROM student 
+WHERE
+    name = 'Van Tai';
